05d80b6c0cee54fab672fa6754c38c663de45bfb
Tero Karvinen <teropkg@gmx.com>
2022-11-05 01:40:35 +0200

    Public release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# palettero
-Command palette - Ctrl-P to fuzzy search &amp; run commands, textfilters and descriptions - Micro editor plugin
+# Palettero - command palette / menu for Micro editor
+
+Command palette for Micro editor - fuzzy search commands and textfilters
+
+The first and original command paletter for Micro. 
+
+![Screenshot](palettero-command-palette-menu-for-micro-editor.png)
+
+# Usage
+
+Press **Ctrl-P** to open Palettero. Type to fuzzy search commands and their descriptions. Use up and down arrows to move. Press enter to select, or Esc to cancel. 
+
+Command is pre-filled to command bar. Edit the command, then press enter to execute. 
+
+Tab completes file names in some commands, such as "tab" and "open". 
+
+# Requirements
+
+Requires: fzf, bash
+
+Only tested on Linux. 
+
+# Adding your own commands
+
+You can add your own commands to $HOME/.config/micro/palettero.cfg
+
+# Adminstrivia
+
+https://TeroKarvinen.com/micro
---
+++ main.lua
@@ -0,0 +1,79 @@
+-- Copyright 2022 Tero Karvinen http://TeroKarvinen.com
+-- Command palette - fuzzy search commands and textfilters
+-- A Micro editor plugin
+
+local micro = import("micro")
+local config = import("micro/config")
+local shell = import("micro/shell")
+local menufile = config.ConfigDir.."/plug/palettero/palettero-defaults.cfg"
+local userfile = config.ConfigDir.."/palettero.cfg"
+
+function init()
+	-- runs once when micro starts
+	config.MakeCommand("palettero", paletteroCommand, config.NoComplete)
+	config.TryBindKey("CtrlP", "command:palettero", false)
+	shell.ExecCommand("touch", userfile)
+end
+
+-- ## Prompt ##
+
+function commandBar(command)
+	-- open ctrl-E command bar, fill it with start of command, allow user to fill the rest
+	micro.InfoBar():Prompt("T> ", command, "Command", nil, promptDoneCallback)
+end
+
+function promptDoneCallback(resp, cancelled)
+	-- called by prompt() when user is done with ctrl-E command bar
+	if cancelled then
+		micro.InfoBar():Message("Command cancelled.")
+	end
+	micro.InfoBar():Message("Running: ", resp)
+	
+	local bp = micro.CurPane()
+	bp:HandleCommand(resp)
+end
+
+-- ## palette command ##
+
+function paletteroCommand(bp)
+	-- ctrl-E palettero
+	micro.InfoBar():Message("Palettero command palette activated!")
+
+n	local showMenuCmd = string.format("bash -c \"cat '%s' '%s'|fzf --layout=reverse\"", menufile, userfile)
+	micro.Log("Requesting user input with: ", showMenuCmd) -- run 'micro --debug tero' to create log.txt
+	local choice = shell.RunInteractiveShell(showMenuCmd, false, true)
+	micro.Log("User chose: ", choice)
+	local cmd = getCommand(choice)
+	micro.Log("Command: ", cmd)
+	if nil == cmd then
+		micro.InfoBar():Message("Empty choice, not running.")
+		return
+	end
+	micro.InfoBar():Message("Running: ", cmd)
+	-- bp:HandleCommand(cmd)
+	commandBar(cmd)
+end
+
+-- ## Menu item collection ##
+
+function getCommand(s)
+	-- return command part of string, or empty if string starts with comment char
+	-- "command # comment" -> "command"
+	-- "# command # comment" -> ""
+	-- "command # comment # comment" -> "command"
+
+	if string.len(s) == 0 then
+		return
+	end
+
+	if "#" == s:sub(1,1) then
+		return "" -- string starts with comment character
+	end
+	
+	local cmd = string.match(s, "([^#]*) ?#")
+	if nil ~= cmd and string.len(cmd) > 0 then
+		return cmd -- string had a comment char, return what was before it
+	end
+
+	return s -- no comment char in string	
+end
